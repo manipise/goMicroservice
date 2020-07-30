5ae7a5c2068487a051f1ef8a3dfe35e384615c46
Manikanta <pise.manikanta@gmail.com>
2020-07-30 14:43:59 +0530

    Create startSerSendReqBack2User.go

---
+++ startSerSendReqBack2User.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello boss")
+	})
+	http.HandleFunc("/sayhi", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello ")
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Hello", string(b))
+		fmt.Fprintf(rw, "Hello %s \n", b)
+	})
+	http.ListenAndServe(":8088", nil)
+}
